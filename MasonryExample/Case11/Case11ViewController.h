654f27efbf4bcb5f324457efd7d2aa9b65df6f1e
zekunyan <zekunyan@163.com>
2016-08-06 21:02:40 +0800

    增加Case11，Autolayout的绑定特性

---
+++ MasonryExample/Case11/Case11ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Case11ViewController.h
+//  MasonryExample
+//
+//  Created by tutuge on 16/8/6.
+//  Copyright © 2016年 tutuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Case11ViewController : UIViewController
+
+@end
--- MasonryExample/Case11/Case11ViewController.m
+++ MasonryExample/Case11/Case11ViewController.m
@@ -1,15 +1,15 @@
 //
-//  Case10ViewController.m
+//  Case11ViewController.m
 //  MasonryExample
 //
 //  Created by tutuge on 16/8/6.
 //  Copyright © 2016年 tutuge. All rights reserved.
 //
 
-#import "Case10ViewController.h"
+#import "Case11ViewController.h"
 #import <Masonry/Masonry.h>
 
-@interface Case10ViewController ()
+@interface Case11ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UILabel *logLabel;
 @property (nonatomic, strong) UILabel *tipLabel;
@@ -18,7 +18,7 @@
 @property (nonatomic, strong) MASConstraint *topConstraint;
 @end
 
-@implementation Case10ViewController
+@implementation Case11ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,10 +27,11 @@
     _containerView.layer.borderWidth = 1.0f;
     _containerView.layer.borderColor = [UIColor redColor].CGColor;
     
-    self.tipLabel.text = @"tutuge.me\niOS";
+    self.tipLabel.text = @"tutuge";
     [_containerView addSubview:self.tipLabel];
     [_tipLabel sizeToFit];
     
+    // 设置主Label的约束
     [_tipLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         // 设置边界条件约束，保证内容可见，优先级1000
         make.left.greaterThanOrEqualTo(_containerView.mas_left);
@@ -44,6 +45,21 @@
         make.height.mas_equalTo(CGRectGetHeight(_tipLabel.frame) + 4);
     }];
     
+    // Attachment，依附在tipLabel右上方
+    UIView *attachmentView = [self generateNewAttachmentLabelWithContent:@" Attachemnt "];
+    [_containerView addSubview:attachmentView];
+    
+    [attachmentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 依附在tipLabel上
+        make.left.equalTo(_tipLabel.mas_left).with.offset(20).priorityHigh();
+        make.bottom.equalTo(_tipLabel.mas_top).with.offset(-2).priorityHigh();
+        
+        // 边界条件
+        make.top.greaterThanOrEqualTo(_containerView.mas_top);
+        make.right.lessThanOrEqualTo(_containerView.mas_right);
+    }];
+    
+    // 拖动Gesture
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panWithGesture:)];
     [_containerView addGestureRecognizer:pan];
 }
@@ -52,6 +68,7 @@
 
 - (void)panWithGesture:(UIPanGestureRecognizer *)pan {
     CGPoint touchPoint = [pan locationInView:_containerView];
+    
     _logLabel.text = NSStringFromCGPoint(touchPoint);
     
     _leftConstraint.offset = touchPoint.x;
@@ -74,6 +91,27 @@
         _tipLabel.layer.cornerRadius = 2.0f;
     }
     return _tipLabel;
+}
+
+- (UILabel *)generateNewAttachmentLabelWithContent:(NSString *)content {
+    UILabel *label = [UILabel new];
+    
+    label.textColor = [UIColor whiteColor];
+    label.font = [UIFont systemFontOfSize:14.0f];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.backgroundColor = [UIColor colorWithRed:0.39 green:0.39 blue:0.39 alpha:1];
+    label.layer.masksToBounds = YES;
+    label.layer.cornerRadius = 2.0f;
+    
+    [label setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [label setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    [label setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [label setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    
+    label.text = content;
+    [label sizeToFit];
+    
+    return label;
 }
 
 @end
