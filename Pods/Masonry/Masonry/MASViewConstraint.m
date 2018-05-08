59f719e72382eb236eb76a9bc68d8bc7d9201344
燕泽堃(燕疏) <zekun.yzk@alibaba-inc.com>
2018-05-08 14:34:52 +0800

    Update Pods

--- Pods/Masonry/Masonry/MASViewConstraint.m
+++ Pods/Masonry/Masonry/MASViewConstraint.m
@@ -1,5 +1,5 @@
 //
-//  MASConstraint.m
+//  MASViewConstraint.m
 //  Masonry
 //
 //  Created by Jonas Budelmann on 20/07/13.
@@ -175,6 +175,7 @@ static char kInstalledConstraintsKey;
             NSMutableArray *children = NSMutableArray.new;
             for (id attr in attribute) {
                 MASViewConstraint *viewConstraint = [self copy];
+                viewConstraint.layoutRelation = relation;
                 viewConstraint.secondViewAttribute = attr;
                 [children addObject:viewConstraint];
             }
@@ -251,6 +252,10 @@ static char kInstalledConstraintsKey;
         default:
             break;
     }
+}
+
+- (void)setInset:(CGFloat)inset {
+    [self setInsets:(MASEdgeInsets){.top = inset, .left = inset, .bottom = inset, .right = inset}];
 }
 
 - (void)setOffset:(CGFloat)offset {
